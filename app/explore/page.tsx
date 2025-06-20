b2e3918ffaa9426ac02a482d84f0fef8792f6b27
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2025-06-20 18:58:52 +0700

    update search di explore

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -169,7 +169,7 @@ export default function ExplorePage() {
           </p>
 
           {/* Search Bar */}
-          <div className="relative max-w-md">
+          <div className="relative">
             <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
               <svg
                 className="h-5 w-5 text-gray-400"
@@ -190,7 +190,7 @@ export default function ExplorePage() {
               placeholder="Search mentors..."
               value={searchQuery}
               onChange={(e) => setSearchQuery(e.target.value)}
-              className="block w-full pl-10 pr-3 py-3 border border-gray-300 rounded-full leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-2 focus:ring-sky-100 focus:border-blue-500 text-black"
+              className=" w-full pl-10 pr-3 py-3 border border-gray-300 rounded-full leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-2 focus:ring-sky-100 focus:border-blue-500 text-black"
             />
           </div>
         </div>
